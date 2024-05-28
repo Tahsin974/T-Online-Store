9daa5d0146658600c4b84f7690a3aafdca5430b3
Tahsin-Islam <tahsinislam974@gmail.com>
2024-05-28 17:47:32 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -16,27 +16,38 @@ const showProducts = (products) => {
    
    setInnerText('total_products', products.length);
 
-   document.getElementById("all-products").innerHTML = "";
+   const allProductsArea = document.getElementById("all-products");
+   allProductsArea.textContent = '';
 
    const allProducts = products.slice(0, 10).map((pd) => pd);
    for (const product of allProducts) {
-      const image = product.images;
+      const image = product.image;
       const div = document.createElement('div');
       div.classList.add('product');
-      div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      div.classList.add('col');
+      div.innerHTML = `
+      <div class="single-product card h-100 text-center">
+               <div>
+               <img class="product-image" src=${image}></img>
+               </div>
+               <div class="card-body">
+                  <h5 >${product.title}</h5>
+               </div>
+               <p>Category: ${product.category}</p>
+               <p  > <span class="fw-bold">Rating:</span> ${product.rating.rate
+               }</p>
+               <p ><span class="fw-bold">Rating-Count:</span> ${product.rating.count
+               }</p>
+               <h2>Price: $ ${product.price}</h2>
 
-      <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
-      data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
-      
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
+               <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
+               data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
+               
+               <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
+         
+      </div>
       `;
-      document.getElementById('all-products').appendChild(div);
+      allProductsArea.appendChild(div);
    }
 };
 
@@ -44,10 +55,11 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
+   updateTotal();
 };
 
 const showProductDetails = (product_id) => {
@@ -60,28 +72,28 @@ const showProductDetails = (product_id) => {
 const showProductDetailsInModal = (product_details) => {
    console.log(product_details);
    setInnerText('exampleModalLabel', product_details.title);
-   setInnerText('product_id', product_details.id);
+   setInnerText('productId', product_details.id);
    setInnerText('modal_body', product_details.description);
    setInnerText('rating', product_details.rating.rate);
 };
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = parseFloat(total).toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-   document.getElementById(id).innerText = Math.round(value);
+   document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
@@ -113,9 +125,10 @@ const updateTotal = () => {
 // search by category
 document.getElementById("search-btn").addEventListener("click", function () {
    const inputField = document.getElementById("input-value").value;
-   const searchedProduct = arr[0].find((p) =>
+   const searchedProduct = arr[0].filter((p) =>
      p.category.startsWith(`${inputField}`)
    );
+
    showProducts(searchedProduct);
  });
 
